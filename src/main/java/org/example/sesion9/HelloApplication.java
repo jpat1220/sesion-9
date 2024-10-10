becb875fdc64003085389f5920ce09b404bfd11d
Jorge <jorge.patino@correounivalle.edu.co>
2024-10-10 16:57:46 -0500

    xxxx commit

--- src/main/java/org/example/sesion9/HelloApplication.java
+++ src/main/java/org/example/sesion9/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("dbdbdbdb");
+        Scene scene = new Scene(fxmlLoader.load(), 320,320 );
+        stage.setTitle("holaaaa");
         stage.setScene(scene);
         stage.show();
     }
